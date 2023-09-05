02cc82bd241b0b70df2035c3e78b1f8cb0269aa9
Russell <125916931+super0xdev@users.noreply.github.com>
2023-09-05 00:08:45 -0400

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -19,7 +19,7 @@ const Footer = () => {
                         </div>
 
                         <div className="w-[35px] h-[35px] rounded-full flex justify-center items-center">
-                            <a href="https://t.me/RussellMasato">
+                            <a href="https://t.me/Russell_Masato">
                                 <FaTelegram className="text-3xl" />
                             </a>
                         </div>
